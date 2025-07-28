2e04b8be3f86b6d19888d81e4a2821cc847375f9
SidW111 <siddhantwainngade@gmail.com>
2025-07-28 16:46:58 +0530

    initialising admin and doctor roles for the projext

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center text-8xl hover:scale-105 transition-all duration-500'>
+      <h1>Hello SId first trye</h1>
+    </div>
+  )
+}
+
+export default App
