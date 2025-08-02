3a33b57b9d6c82cfd13d9c8eb87c112072b1e52c
SidW111 <siddhantwainngade@gmail.com>
2025-08-03 02:26:56 +0530

    added sidebar for admin

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -1,36 +1,66 @@
-import { NavLink } from "react-router-dom"
-import { useAdminContext } from "../context/AdminContext"
+import { NavLink } from "react-router-dom";
+import { useAdminContext } from "../context/AdminContext";
 import { RxDashboard } from "react-icons/rx";
-
+import { RiCalendarScheduleLine } from "react-icons/ri";
+import { MdOutlineGroupAdd } from "react-icons/md";
+import { PiUserList } from "react-icons/pi";
 import { useEffect } from "react";
 
-const SideBar = () =>{
+const SideBar = () => {
+  const { aToken } = useAdminContext();
 
-    const {aToken} = useAdminContext();
-
-
-
-    return <div className="min-h-screen bg-white border-r">
-       {
-        aToken && 
-            <ul className="text-[#515151] mt-5 ">
-                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : '' }`} to={'/admin-dashboard'}>
-                {/* <img src={assets.home_icon} alt="" /> */}
-                <p className='hidden md:block'>Dashboard</p>
-            </NavLink>
-                <NavLink className={'md:min-w-72 md:px-9 px-3 py-3.5 gap-3 flex '}>
-                    Appointments
-                </NavLink>
-                <NavLink className={"flex px-3 py-3.5 md:px-9"}>
-                    Add Doctors
-                </NavLink>
-                <NavLink>
-                    Doctors List
-                </NavLink>
-            </ul>
-        
-       }
+  return (
+    <div className="min-h-screen bg-white border-r">
+      {aToken && (
+        <ul className="text-[#515151] mt-5 ">
+          <NavLink
+            className={({ isActive }) =>
+              `font-medium flex gap-3 md:px-9 items-center px-3 py-3.5 min-w-72 ${
+                isActive ? "bg-blue-50 border-r-4 border-blue-500 " : " "
+              }`
+            }
+            to={"/admin-dashboard"}
+          >
+            <RxDashboard size={22} />
+            <p className="hidden md:block">Dashboard</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `font-medium flex items-center gap-3 md:px-9 px-3 py-3.5 min-w-72 ${
+                isActive ? "bg-blue-50 border-r-4 border-blue-500" : ""
+              }`
+            }
+            to={"/appointments"}
+          >
+            <RiCalendarScheduleLine size={22}/>
+            <p className="md:block hidden">Appointment</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `font-medium flex items-center gap-3 md:px-9 px-3 py-3.5 min-w-72 ${
+                isActive ? "bg-blue-50 border-r-4 border-blue-500" : ""
+              }`
+            }
+            to={"/add-doctor"}
+          >
+            <MdOutlineGroupAdd  size={22}/>
+            <p className="md:block hidden">Add Doctor</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `font-medium flex items-center gap-3 md:px-9 px-3 py-3.5 min-w-72 ${
+                isActive ? "bg-blue-50 border-r-4 border-blue-500" : ""
+              }`
+            }
+            to={"/doctors-list"}
+          >
+            <PiUserList  size={22}/>
+            <p className="md:block hidden">Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
     </div>
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
