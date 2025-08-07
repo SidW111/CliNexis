141ff6443556ca482905d82f5a33950ca1606c89
Siddhant Waingade <111293278+SidW111@users.noreply.github.com>
2025-08-07 14:27:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ connectCloudinary()
 app.use(express.json())
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://clinexis-fe.onrender.com",
   credentials:true
 }))
 app.use(cookieParser())
