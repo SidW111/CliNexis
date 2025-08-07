b284e4d92ac905dad602ae2932bc8b855fd62200
Siddhant Waingade <111293278+SidW111@users.noreply.github.com>
2025-08-07 14:12:22 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AppProvider } from '../../admin/src/context/AppContext.jsx'
+import { AppProvider } from '../src/context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
